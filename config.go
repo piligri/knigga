8752526ed2a80f9e3606a2b094784b6e985f2ee6
Andrey <piligri21@gmail.com>
2026-03-06 14:51:13 +0300

    изменено:      config.go 	изменено:      config.yaml 	изменено:      opchmi.go

--- config.go
+++ config.go
@@ -9,8 +9,10 @@ import (
 
 // Структура глобальной конфигурации приложения
 type GlobalConfig struct {
-	LineCfg `yaml:",inline"` //Конфигурация линии
-	OnecCfg `yaml:",inline"` //Конфигурация 1С
+	LineCfg  `yaml:",inline"` //Конфигурация линии
+	OnecCfg  `yaml:",inline"` //Конфигурация 1С
+	QRdata   `yaml:",inline"` //Конфигурация тегов данных QR
+	QRstatus `yaml:",inline"` //Конфигурация тегов статуса QR
 }
 
 // Структура описания конфигурации подключения к 1с
@@ -26,6 +28,33 @@ type LineCfg struct {
 	LineIP   string `yaml:"LineIP" validate:"ip"`    //Сетевой адрес линии
 	LineName string `yaml:"LineName"`                //Название линии
 	LineUID  string `yaml:"LineUID" validate:"uuid"` //UID линии в 1с
+}
+
+// Структура описания тегов данных QR
+type QRdata struct {
+	Fio      string `yaml:"Fio"`      //ФИО исполнителя
+	Etap     string `yaml:"Etap"`     //Номер Этапа
+	Operacia string `yaml:"Operacia"` //Строка операции
+	Rc       string `yaml:"Rc"`       //Наименование рабочего центра
+	Uchastok string `yaml:"Uchastok"` //Участок исполнителя
+	PlanM    string `yaml:"PlanM"`    //Метраж из заявки на этап
+	FaktM    string `yaml:"FaktM"`    //Фактическая выработка из счетчика линии
+	QRFiao   string `yaml:"QRFiao"`   //Считанный и обработанный QR Исполнителя
+	QREtap   string `yaml:"QREtap"`   //Считанный и обработанный QR Этапа
+}
+
+// Структура описания тегов статусов QR
+type QRstatus struct {
+	InitQRWindow        string `yaml:"InitQRWindow"`        //Окно считывания QR кода открыто
+	FioReadComplite     string `yaml:"FioReadComplite"`     //Код QR ФИО считан
+	EtapReadComplite    string `yaml:"EtapReadComplite"`    //Код QR Этапа считан
+	EtapProcess         string `yaml:"EtapProcess"`         //Этап в работе - TRUE, этап не начат FALSE
+	EtapEnd             string `yaml:"EtapEnd"`             //Этап завершен
+	AllDataComplite     string `yaml:"AllDataComplite"`     //Все данные считаны и готовы к записи в 1с
+	AllDataReadytoWrite string `yaml:"AllDataReadytoWrite"` //?????
+	NextQRAccept        string `yaml:"NextQRAccept"`        //Разрешение считывания QR (инверсное поле, FALSE - разрешено чтение)
+	ScanerQREnable      string `yaml:"ScanerQREnable"`      //Разрешение работы сканера
+	QR                  string `yaml:"QR"`                  //Считанный QR
 }
 
 // Создание базовой конфигурации приложения
--- opchmi.go
+++ opchmi.go
@@ -12,31 +12,6 @@ var qrdata [9]string = [9]string{
 	"s=Main_QRdata_QRFio",    // 8
 }
 
-type QRdata struct {
-	Fio      string  //ФИО исполнителя
-	Etap     string  //Номер Этапа
-	Operacia string  //Строка операции
-	Rc       string  //Наименование рабочего центра
-	Uchastok string  //Участок исполнителя
-	PlanM    float32 //Метраж из заявки на этап
-	FaktM    string  //Фактическая выработка из счетчика линии
-	QRFiao   string  //Считанный и обработанный QR Исполнителя
-	QREtap   string  //Считанный и обработанный QR Этапа
-}
-
-type QRstatus struct {
-	InitQRWindow        bool   //Окно считывания QR кода открыто
-	FioReadComplite     bool   //Код QR ФИО считан
-	EtapReadComplite    bool   //Код QR Этапа считан
-	EtapProcess         bool   //Этап в работе - TRUE, этап не начат FALSE
-	EtapEnd             bool   //Этап завершен
-	AllDataComplite     bool   //Все данные считаны и готовы к записи в 1с
-	AllDataReadytoWrite bool   //?????
-	NextQRAccept        bool   //Разрешение считывания QR (инверсное поле, FALSE - разрешено чтение)
-	ScanerQREnable      bool   //Разрешение работы сканера
-	QR                  string //Считанный QR
-}
-
 var qrstatus [10]string = [10]string{
 	"s=Main_QRStatus_QRBa", //0 Окно считывания QR открыто
 	"s=Main_QRStatus_QRBb", //1 Считан ФИО
